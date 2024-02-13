7f67b9a85f45909dfc2bda1ee03708adecc11e7d
jonnhyOrtega <jonnhyortega@gmail.com>
2024-02-13 11:37:03 -0300

    Se agrego barra desplegable en parte superior

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,26 @@
-const indicator = document.querySelector('.indicator span');
+// BARRA DE SCROLL SUPERIOR
+
+const indicator = document.querySelector(".indicator span");
 
 const progressBar = () => {
-    const { scrollTop, scrollHeight } = document.documentElement;
-    const scrollPercent = (scrollTop / (scrollHeight - window.innerHeight)) * 100;
+  const { scrollTop, scrollHeight } = document.documentElement;
+  const scrollPercent = (scrollTop / (scrollHeight - window.innerHeight)) * 100;
 
-    indicator.style.width = `${scrollPercent}%`;
+  indicator.style.width = `${scrollPercent}%`;
 };
 
-window.addEventListener('scroll', progressBar);
+window.addEventListener("scroll", progressBar);
+
+// FUNCION PARA ABRIR MENU
+let topDeMenu = document.getElementById("header");
+let botonOpenMenu = document.getElementById("open_header");
+
+const abrirCerrarMenu = () => {
+  if (topDeMenu.style.top === "-13%") {
+    topDeMenu.style.top = "0";
+    botonOpenMenu.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
+  } else if ((topDeMenu.style.top = "0")) {
+    topDeMenu.style.top = "-13%";
+    botonOpenMenu.innerHTML = '<i class="fa-solid fa-arrow-down"></i>';
+  }
+};
