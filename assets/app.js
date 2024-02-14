fd61a3565d4c411e2a31762b6570341e9c26e03c
jonnhyOrtega <jonnhyortega@gmail.com>
2024-02-14 08:21:54 -0300

    se agrego menu desplegable

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-// BARRA DE SCROLL SUPERIOR
+// // BARRA DE SCROLL SUPERIOR
 
 const indicator = document.querySelector(".indicator span");
 
@@ -14,7 +14,6 @@ window.addEventListener("scroll", progressBar);
 // FUNCION PARA ABRIR MENU
 let topDeMenu = document.getElementById("header");
 let botonOpenMenu = document.getElementById("open_header");
-
 const abrirCerrarMenu = () => {
   if (topDeMenu.style.top === "-13%") {
     topDeMenu.style.top = "0";
@@ -23,4 +22,25 @@ const abrirCerrarMenu = () => {
     topDeMenu.style.top = "-13%";
     botonOpenMenu.innerHTML = '<i class="fa-solid fa-arrow-down"></i>';
   }
+};
+
+let leftMenu = document.getElementById("header_mobiles");
+const abrirCerrarMenuMobiles = () => {
+  if (leftMenu.style.left === "-10%") {
+    leftMenu.style.left = "0";
+    botonOpenMenu.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
+  } else if ((leftMenu.style.left = "0")) {
+    leftMenu.style.left = "-10%";
+    botonOpenMenu.innerHTML = '<i class="fa-solid fa-arrow-left"></i>';
+  }
+};
+
+// FUNCION PARA MOSTRAR #BIO-MENSAJE
+
+const maquinaEscribir = () => {
+  let bioMensaje = document.getElementById("bio-mensaje");
+  let contenido =
+    " Después de años de dedicación al deporte, canalice mi energía y determinación hacia el mundo del desarrollo web. Buscando innovar condiseños armónicos y originales.";
+
+  bioMensaje.innerHTML = contenido;
 };
